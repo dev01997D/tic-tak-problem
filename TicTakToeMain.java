e992281988bb5e7cc351b6a47910e5d6c37a4b3f
dev01997D <devnandan01997@gmail.com>
2020-10-03 14:33:49 +0530

    Added: Ability to allow user to choose option for input functionality added

--- TicTakToeMain.java
+++ TicTakToeMain.java
@@ -1,21 +1,43 @@
 package com.blz.tictaktoeproblem;
 
+import java.util.Scanner;
+
 public class TicTakToeMain {
-       //Creating method CreateBoard
-  	private void createBoard() {
+	private static Scanner sc = new Scanner(System.in);
+
+	// Creating method CreateBoard
+	private char[] createBoard() {
 		// create board and assign it with empty space
 		char[] board = new char[10];
 		for (int indexBoard = 0; indexBoard < 10; indexBoard++) {
 			if (indexBoard != 0)
 				board[indexBoard] = ' ';
 		}
+		return board;
 	}
-	
+
+	// method to allow player choose 'X' or 'O'
+	private char allowPlayerChooseOption(char userOption) {
+		char computerOption;
+		if (userOption == 'O')
+			computerOption = 'X';
+		else
+			computerOption = 'O';
+		System.out.println("Computer option is : " + computerOption + "\n");
+		return computerOption;
+	}
+
 	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tak Toe game");
 		// Creating object of class
 		TicTakToeMain ticObj = new TicTakToeMain();
 
 		// Invoking createBoard Method
-		ticObj.createBoard();
+		char board[] = ticObj.createBoard();
+
+		// Invoking allowChooseOptionMethod
+		System.out.println("Enter the option 'X' or 'O'");
+		char option = sc.next().charAt(0);
+		ticObj.allowPlayerChooseOption(option);
 	}
 }
